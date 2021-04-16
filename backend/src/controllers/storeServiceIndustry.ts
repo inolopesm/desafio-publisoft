75f147c3f4dfe517735c00685e1cb7f6690a447d
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-04-16 19:45:13 -0300

    adiciona query para guardar serviceIndustry em storeServiceIndustry

--- backend/src/controllers/storeServiceIndustry.ts
+++ backend/src/controllers/storeServiceIndustry.ts
@@ -1,20 +1,7 @@
 import { RequestHandler } from 'express'
-
-interface StoreServiceIndustryDTO {
-  type: string
-  register: string
-  name: string
-  email: string
-  zipCode: string
-  street: string
-  number: string
-  complement: string
-  neighborhood: string
-  city: string
-  state: string
-}
+import knex from '../database/knex'
 
 export const storeServiceIndustry: RequestHandler = async (req, res) => {
-  const data: StoreServiceIndustryDTO = req.body
+  await knex.client('serviceIndustry').insert(req.body)
   return res.status(201).end()
 }
