f5d0beef824a8c64bad09a67cef60e6bf023cadc
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-04-17 15:33:52 -0300

    frontend: adiciona rota para dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import './styles/global.css'
 import CreateServiceIndustry from './pages/CreateServiceIndustry'
 import CreateContract from './pages/CreateContract'
 import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
+        <Route path="/" exact>
+          <Redirect to="/dashboard" />
+        </Route>
+
+        <Route path="/dashboard" component={Dashboard} />
         <Route path="/serviceIndustries/create" component={CreateServiceIndustry} />
         <Route path="/contracts/create" component={CreateContract} />
       </Switch>
