b34213cbd3044d0d9429bb2e0b8289e67599a0e0
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-04-17 14:12:52 -0300

    backend: adiciona listContractsController nas rotas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { listContractsController } from './controllers/listContractsController'
 import { listServiceIndustryController } from './controllers/listServiceIndustryController'
 import { storeContractController } from './controllers/storeContractController'
 import { storeServiceIndustryController } from './controllers/storeServiceIndustryController'
@@ -8,5 +9,6 @@ const routes = Router()
 routes.post('/serviceIndustries', storeServiceIndustryController)
 routes.get('/serviceIndustries', listServiceIndustryController)
 routes.post('/contracts', storeContractController)
+routes.get('/contracts', listContractsController)
 
 export default routes
