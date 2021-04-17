edba4c3e3d656fcebd2000207d98b10646c0c9a8
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-04-17 18:25:22 -0300

    frontend: ajuda app para o antd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import './styles/global.css'
+import { Layout } from 'antd'
+import 'antd/dist/antd.css'
 import CreateServiceIndustry from './pages/CreateServiceIndustry'
 import CreateContract from './pages/CreateContract'
 import Header from './components/Header'
@@ -8,18 +9,21 @@ import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/dashboard" />
-        </Route>
-
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/serviceIndustries/create" component={CreateServiceIndustry} />
-        <Route path="/contracts/create" component={CreateContract} />
-      </Switch>
-    </Router>
+    <Layout>
+      <Router>
+        <Header />
+        <Layout.Content>
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/dashboard" />
+            </Route>
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/serviceIndustries/create" component={CreateServiceIndustry} />
+            <Route path="/contracts/create" component={CreateContract} />
+          </Switch>
+        </Layout.Content>
+      </Router>
+    </Layout>
   )
 }
 
