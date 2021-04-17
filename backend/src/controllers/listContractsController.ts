cdf677d0258c9187d1018e004d74e8592b123542
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-04-17 16:53:00 -0300

    backend: altera organizacao dos dados de saida de listContractsController

--- backend/src/controllers/listContractsController.ts
+++ backend/src/controllers/listContractsController.ts
@@ -2,12 +2,26 @@ import { RequestHandler } from 'express'
 import knex from '../database/knex'
 
 export const listContractsController: RequestHandler = async (_req, res) => {
-  const contractDTOs = await knex.client('contract')
+  const rows = await knex.client('contract')
     .select([
       'contract.id', 'contract.serviceIndustryId', 'contract.startDate', 'contract.endDate',
       'serviceIndustry.name', 'serviceIndustry.register'
     ])
     .innerJoin('serviceIndustry', 'contract.serviceIndustryId', 'serviceIndustry.id')
     .whereNull('contract.deletedAt')
+
+  const contractDTOs = rows.map(row => {
+    return {
+      id: row.id,
+      startDate: row.startDate,
+      endDate: row.endDate,
+      serviceIndustry: {
+        id: row.serviceIndustryId,
+        name: row.name,
+        register: row.register
+      }
+    }
+  })
+
   return res.json(contractDTOs)
 }
