5ec39fc7eb8268c818bf682fb7f7942009198358
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-04-17 18:25:35 -0300

    frontend: ajusta header para o antd

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { Layout, Menu } from 'antd'
 
 export default function Header() {
+  const { pathname } = useLocation()
+
   return (
-    <header>
-      <h1>Publicsoft</h1>
-      <nav>
-        <NavLink to="/dashboard">Painel Visual</NavLink>
-        &nbsp;|&nbsp;
-        <NavLink to="/serviceIndustries/create">Criar Prestador de Serviço</NavLink>
-        &nbsp;|&nbsp;
-        <NavLink to="/contracts/create">Criar Contrato</NavLink>
-      </nav>
-    </header>
+    <Layout.Header>
+      <img src="/logo.png" alt="Logo" style={{ display: 'inline-block', float: 'left', margin: '1rem 2rem 1rem 0' }} />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['/']} selectedKeys={[pathname]}>
+        <Menu.Item key="/dashboard">
+          <Link to="/dashboard">Painel Visual</Link>
+        </Menu.Item>
+        <Menu.Item key="/serviceIndustries/create">
+          <Link to="/serviceIndustries/create">Criar Prestador de Serviço</Link>
+        </Menu.Item>
+        <Menu.Item key="/contracts/create">
+          <Link to="/contracts/create">Criar Contrato</Link>
+        </Menu.Item>
+      </Menu>
+    </Layout.Header>
   )
 }
