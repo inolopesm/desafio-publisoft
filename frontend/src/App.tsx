f73cf1419f945f19a5d2661414c68caa135097b8
Matheus Inocencio Lopes <inolopesm@gmail.com>
2021-04-17 15:09:21 -0300

    frontend: organiza paginas em rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './styles/global.css'
 import CreateServiceIndustry from './pages/CreateServiceIndustry'
 import CreateContract from './pages/CreateContract'
+import Header from './components/Header'
 
 function App() {
   return (
-    <>
-      <CreateServiceIndustry />
-      <CreateContract />
-    </>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/serviceIndustries/create" component={CreateServiceIndustry} />
+        <Route path="/contracts/create" component={CreateContract} />
+      </Switch>
+    </Router>
   )
 }
 
